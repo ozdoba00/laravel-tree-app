4a757fa045215941126f9f13e010545c99ba374d
ozdoba00 <ozdoba089@gmail.com>
2022-01-14 16:02:19 +0100

    jwt auth, tree table

---
+++ database/migrations/2022_01_14_143709_create_tree_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTreeTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tree', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('parent_id')->nullable();
+            $table->string('name');
+            $table->boolean('is_node');
+            $table->foreign('parent_id')->references('id')->on('tree')->onDelete('cascade');
+        });
+
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tree');
+    }
+}
